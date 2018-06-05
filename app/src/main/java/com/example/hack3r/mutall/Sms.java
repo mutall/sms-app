def6e43b43bab6174926ee365de2741524fe2ede
amshelhack3r <amshelhack3r@gmail.com>
2018-06-05 11:54:51 +0300

    feat add an sms object to initialize an sms number and body

---
+++ app/src/main/java/com/example/hack3r/mutall/Sms.java
@@ -0,0 +1,22 @@
+package com.example.hack3r.mutall;
+
+public class Sms {
+    private String smsBody;
+    private String smsNumber;
+
+    public String getSmsBody() {
+        return smsBody;
+    }
+
+    public String getSmsNumber() {
+        return smsNumber;
+    }
+
+    public void setSmsBody(String smsBody) {
+        this.smsBody = smsBody;
+    }
+
+    public void setSmsNumber(String smsNumber) {
+        this.smsNumber = smsNumber;
+    }
+}
